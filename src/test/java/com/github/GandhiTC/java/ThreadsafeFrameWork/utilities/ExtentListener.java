7f57114f9291a94bcaf610d1d8f2e8da724206f3
GandhiTC <GandhiTC@gmail.com>
2020-02-26 20:11:27 -0600

    removed some unused/commented lines

--- src/test/java/com/github/GandhiTC/java/ThreadsafeFrameWork/utilities/ExtentListener.java
+++ src/test/java/com/github/GandhiTC/java/ThreadsafeFrameWork/utilities/ExtentListener.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ISuite;
@@ -64,7 +63,6 @@ public class ExtentListener implements ITestListener, ISuiteListener
 	public void onTestFailure(ITestResult result)
 	{
 		ITestContext 	testContext	= result.getTestContext();
-//						logger 		= (Logger)testContext.getAttribute("BaseLogger");
 						logText		= "<b>TEST CASE FAILED : " + methodName + "</b>";
 						markup		= MarkupHelper.createLabel(logText, ExtentColor.RED);
 
@@ -204,7 +202,6 @@ public class ExtentListener implements ITestListener, ISuiteListener
 	@Override
 	public void onFinish(ITestContext context)
 	{
-//		logger 		= (Logger)context.getAttribute("BaseLogger");
 		logger.error("^^");
 		
 		if(report != null)
