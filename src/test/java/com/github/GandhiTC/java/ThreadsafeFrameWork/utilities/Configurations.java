fbc25f3e41095669f059047d864744b5e9454eb9
GandhiTC <GandhiTC@gmail.com>
2020-07-22 01:35:49 -0500

    code cosmetics only

--- src/test/java/com/github/GandhiTC/java/ThreadsafeFrameWork/utilities/Configurations.java
+++ src/test/java/com/github/GandhiTC/java/ThreadsafeFrameWork/utilities/Configurations.java
@@ -6,15 +6,17 @@ public class Configurations
 {
 	private 	static 	final 	ConfigReader	readconfig			= new ConfigReader();
 	
-	//	The following 3 values are now retrieved from a MySQL database
+	//	The following 3 values are currently retrieved from a MySQL database
 	//	Refer to getCredentialsFromDatabase() method in BaseClass.java
-//	protected	static 	final 	String			baseURL				= readconfig.getApplicationURL();
-//	protected 	static 	final 	String			username			= readconfig.getUsername();
-//	protected	static 	final 	String			password			= readconfig.getPassword();
-	
 	protected	static 		 	String			baseURL				= "";
 	protected 	static 		 	String			username			= "";
 	protected	static 		 	String			password			= "";
+	
+	//	To get their values from a properties file instead
+	//	Comment out previous 3 values and uncomment next 3 lines
+//	protected	static 	final 	String			baseURL				= readconfig.getApplicationURL();
+//	protected 	static 	final 	String			username			= readconfig.getUsername();
+//	protected	static 	final 	String			password			= readconfig.getPassword();
 	
 	protected 	static 	final 	String			firefoxPath			= readconfig.getFirefoxPath();
 	protected 	static 	final 	String			iePath				= readconfig.getIEPath();
